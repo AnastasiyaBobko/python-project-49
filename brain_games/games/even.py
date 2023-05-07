3cb0c8ebaaf4fd86e23d348db260e31183ae4299
AnastasiyaBobko <a.s.bobko@gmail.com>
2023-05-07 23:03:58 +0300

    fix even.py 3

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,14 +4,13 @@ import brain_games.cli
 
 
 def even_game():
-    # print('Welcome to the Brain Games!')
-    # print(f'Hello, {brain_games.cli.welcome_user()}!')
+    print('Welcome to the Brain Games!')
+    print(f'Hello, {brain_games.cli.welcome_user()}!')
     count = 0
 
     while count < 3:
         number = random.randrange(1, 100)
-        print(f"""Answer "yes" if the number is even,
-        otherwise answer "no".\n Question: {number}""")
+        print(f"""Answer "yes" if the number is even, otherwise answer "no".\n Question: {number}""")
         answer = prompt.string('Your answer: ')
         if (number % 2 == 0 and answer == 'yes') or (number % 2 != 0 and answer == 'no'):
             print("Correct!")
@@ -21,4 +20,3 @@ def even_game():
         else:
             print(f"""'{answer}' is wrong answer ;(. \n
             Let's try again, {brain_games.cli.user_name}!""")
-even_game()
