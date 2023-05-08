ecb970a6bd365cd5db96bd2e43a281923971c76b
AnastasiyaBobko <a.s.bobko@gmail.com>
2023-05-08 17:38:16 +0300

    make prime-game code easier

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -3,6 +3,13 @@ import prompt
 import brain_games.cli
 
 
+def IsPrime(num):
+    d = 2
+    while d * d <= num and num % d != 0:
+        d += 1
+        return d * d > num
+
+
 def prime_game():
     print('Welcome to the Brain Games!')
     print(f'Hello, {brain_games.cli.welcome_user()}!')
@@ -10,13 +17,7 @@ def prime_game():
 
     while count < 3:
         num = random.randrange(1, 100)
-
-        def IsPrime(num):
-            d = 2
-            while d * d <= num and num % d != 0:
-                d += 1
-            return d * d > num
-
+        IsPrime(num)
         print(f"""Answer "yes" if given number is prime. Otherwise answer "no".
         Question: {num}""")
         answer = prompt.string('Your answer: ')
@@ -26,19 +27,14 @@ def prime_game():
         else:
             result = 'no'
 
-        if (IsPrime(num) is True and answer == 'yes'):
+        if answer == result:
             print("Correct!")
-            count += 1
-            if count == 3:
-                print(f'Congratulations, {brain_games.cli.user_name}!')
-        elif (IsPrime(num) is False and answer == 'no'):
-            print("Correct!")
-            count += 1
-            if count == 3:
-                print(f'Congratulations, {brain_games.cli.user_name}!')
         else:
             print(f"""'{answer}' is wrong answer ;(.
             Correct answer was {result} \n
             Let's try again, {brain_games.cli.user_name}!""")
             count = 0
             break
+        count += 1
+    if count == 3:
+        print(f'Congratulations, {brain_games.cli.user_name}!')
